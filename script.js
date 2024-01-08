8a047f9ce821984d619c0a20f17dea352a0f5c27
MatteoCantafio25 <mattyteo004@gmail.com>
2024-01-08 16:29:16 +0100

    fridge exercise completed

--- script.js
+++ script.js
@@ -7,29 +7,43 @@ console.log("JS OK");
 // CREO UNA COSTANTE PER L'ARRAY
 
 const fridge = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
-console.log(fridge);
+
 
 //---------------------------------//
 //AGGIUNGIAMO UN ELEMENTO ALL'ARRAY
 
 fridge.push("pesca");
-console.log(fridge);
+
+
+//---------------------------------//
+//CREIAMO UN FLAG
+let hasWatermelon = false;
 
 //---------------------------------//
 // FACCIO CONTROLLARE TUTTI GLI ELEMENTI DELL'ARRAY
 
-
-for (let i = 0; i < fridge.length; i++) {
-    const currentIngredient = fridge[i];
+for (let i = 0; i < fridge.length && !hasWatermelon; i++) {
 
     //----------------//
     // SE IL COCOMERO C'E'STAMPO IN PAGINA...
 
-    if (currentIngredient == fridge[6]) {
-        console.log("Trovato! Devo solo preparare il cocktail.")
+    if (fridge[i] === "cocomero") {
+        hasWatermelon = true
     }
-
-    // SE IL COCOMERO NON C'E'STAMPO IN PAGINA...
-
 }
+
+// SE IL COCOMERO NON C'E'STAMPO IN PAGINA...
+
+let message;
+
+if (hasWatermelon) {
+    message = "Trovato! Devo solo preparare il cocktail."
+} else {
+    message = "Oh no, devo uscire a comprare il cocomero!"
+}
+
+console.log(message);
+
+
+
 
